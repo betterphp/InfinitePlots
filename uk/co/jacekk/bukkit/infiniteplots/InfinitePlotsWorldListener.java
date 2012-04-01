7e7eaf1231cbdf92701148c96b919f2299954c04
Jacek <jacek@kuzemczak.co.uk>
2012-04-01 23:54:48 +0100

    World type is now set to flat to remove swamp grass.

---
+++ uk/co/jacekk/bukkit/infiniteplots/InfinitePlotsWorldListener.java
@@ -0,0 +1,46 @@
+package uk.co.jacekk.bukkit.infiniteplots;
+
+import java.lang.reflect.Field;
+
+import net.minecraft.server.WorldServer;
+import net.minecraft.server.WorldType;
+
+import org.bukkit.World;
+import org.bukkit.craftbukkit.CraftWorld;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.world.WorldInitEvent;
+
+public class InfinitePlotsWorldListener implements Listener {
+	
+	private InfinitePlots plugin;
+	
+	public InfinitePlotsWorldListener(InfinitePlots plugin){
+		this.plugin = plugin;
+	}
+	
+	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+	public void onWorldInit(WorldInitEvent event){
+		World world = event.getWorld();
+		
+		if (world.getGenerator() instanceof InfinitePlotsGenerator){
+			WorldServer worldServer = ((CraftWorld) world).getHandle();
+			
+			try{
+				Class<?> worldData = worldServer.worldData.getClass();
+				
+				Field type = worldData.getDeclaredField("type");
+				type.setAccessible(true);
+				
+				type.set(worldServer.worldData, WorldType.FLAT);
+				
+				plugin.log.info("Changed the world type of '" + world.getName() + "' to flat (this makes the void blue down to y = 0).");
+			}catch (Exception e){
+				plugin.log.info("Could not change the world type of '" + world.getName() + "'.");
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
