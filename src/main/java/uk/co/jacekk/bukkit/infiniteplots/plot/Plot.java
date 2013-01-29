89c707403ca83cc6e061d40fd7ba3108ad78b16c
Jacek <jacek@kuzemczak.co.uk>
2013-01-29 23:18:07 +0000

    Added missing methods to the plot object.

--- src/main/java/uk/co/jacekk/bukkit/infiniteplots/plot/Plot.java
+++ src/main/java/uk/co/jacekk/bukkit/infiniteplots/plot/Plot.java
@@ -1,5 +1,8 @@
 package uk.co.jacekk.bukkit.infiniteplots.plot;
 
+import java.util.Arrays;
+import java.util.List;
+
 import uk.co.jacekk.bukkit.baseplugin.v8.BaseObject;
 import uk.co.jacekk.bukkit.baseplugin.v8.config.PluginConfig;
 import uk.co.jacekk.bukkit.infiniteplots.InfinitePlots;
@@ -41,6 +44,60 @@ public class Plot extends BaseObject<InfinitePlots> {
 	 */
 	public void setAdmin(String admin){
 		this.config.set(PlotConfig.AUTH_ADMIN_NAME, admin);
+	}
+	
+	/**
+	 * Gets all of the players (not including the plot admin) that are allowed to build in tis plot.
+	 * 
+	 * @return The list of player names.
+	 */
+	public List<String> getBuilders(){
+		return this.config.getStringList(PlotConfig.AUTH_BUILDER_NAMES);
+	}
+	
+	/**
+	 * Adds a builder to this plot.
+	 * 
+	 * @param playerName The name of the player to add.
+	 */
+	public void addBuilder(String playerName){
+		List<String> builders = this.getBuilders();
+		builders.add(playerName.toLowerCase());
+		this.config.set(PlotConfig.AUTH_BUILDER_NAMES, builders);
+	}
+	
+	/**
+	 * Remvoed a builder from this plot.
+	 * 
+	 * @param playerName The name of the player to remove.
+	 */
+	public void removeBuilder(String playerName){
+		List<String> builders = this.getBuilders();
+		builders.remove(playerName.toLowerCase());
+		this.config.set(PlotConfig.AUTH_BUILDER_NAMES, builders);
+	}
+	
+	/**
+	 * Removes all builders from this plot.
+	 */
+	public void removeAllBuilders(){
+		this.config.set(PlotConfig.AUTH_BUILDER_NAMES, Arrays.asList(new String[0]));
+	}
+	
+	/**
+	 * Checks to see if a player can build in this plot.
+	 * 
+	 * <p>
+	 * 	A player that is not able to build should not be able to
+	 * 	interact with the environment in any way at all inside
+	 * 	the plot area.
+	 * <p>
+	 * 
+	 * @param playerName The name of the player to test.
+	 * @return True if the player can build, false if not.
+	 */
+	public boolean canBuild(String playerName){
+		return this.getAdmin().equalsIgnoreCase(playerName) || this.getBuilders().contains(playerName);
 	}
 	
 }
