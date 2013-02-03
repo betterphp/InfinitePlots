b5061cccd572b541b7cc0055cc04397f9747a32f
Jacek <jacek@kuzemczak.co.uk>
2013-02-03 03:16:52 +0000

    Set default path block to a double slab.

--- src/main/java/uk/co/jacekk/bukkit/infiniteplots/Config.java
+++ src/main/java/uk/co/jacekk/bukkit/infiniteplots/Config.java
@@ -12,13 +12,13 @@ public class Config {
 	public static final PluginConfigKey GRID_SIZE				= new PluginConfigKey("plots.size",				32);
 	public static final PluginConfigKey GRID_HEIGHT				= new PluginConfigKey("plots.height",			20);
 	
-	public static final PluginConfigKey BLOCKS_PATH				= new PluginConfigKey("blocks.path",			Material.STEP.getId());
+	public static final PluginConfigKey BLOCKS_PATH				= new PluginConfigKey("blocks.path",			Material.DOUBLE_STEP.getId());
 	public static final PluginConfigKey BLOCKS_PATH_DATA		= new PluginConfigKey("blocks.path-data", 		0);
 	public static final PluginConfigKey BLOCKS_LOWER_WALL		= new PluginConfigKey("blocks.lower-wall",		Material.SMOOTH_BRICK.getId());
 	public static final PluginConfigKey BLOCKS_LOWER_WALL_DATA	= new PluginConfigKey("blocks.lower-wall-data",	0);
 	public static final PluginConfigKey BLOCKS_UPPER_WALL		= new PluginConfigKey("blocks.upper-wall",		Material.FENCE.getId());
 	public static final PluginConfigKey BLOCKS_UPPER_WALL_DATA	= new PluginConfigKey("blocks.upper-wall-data",	0);
-
+	
 	public static final PluginConfigKey RESTRICT_SPAWNING		= new PluginConfigKey("restrict-spawning",		false);
 	
 }
