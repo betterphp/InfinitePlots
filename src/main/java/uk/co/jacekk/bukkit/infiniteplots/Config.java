e50771f547bfcdab90fc6876fcc3a6a3070c53e5
Jacek <jacek@kuzemczak.co.uk>
2013-09-11 12:52:41 +0100

    Moved config option.

--- src/main/java/uk/co/jacekk/bukkit/infiniteplots/Config.java
+++ src/main/java/uk/co/jacekk/bukkit/infiniteplots/Config.java
@@ -15,13 +15,13 @@ public class Config {
 	public static final PluginConfigKey USE_SIGNS				= new PluginConfigKey("use-signs",				true);
 	public static final PluginConfigKey TRACK_STATS				= new PluginConfigKey("track-stats",			true);
 	public static final PluginConfigKey OWNER_PREFIX			= new PluginConfigKey("owner-prefix",			"Plot Owner");
+	public static final PluginConfigKey PURGE_SAFE_LIST			= new PluginConfigKey("purge-safe-list",		new String[0]);
 	public static final PluginConfigKey RESET_DELAY				= new PluginConfigKey("reset.delay",			5);
 	public static final PluginConfigKey RESET_PERTICK			= new PluginConfigKey("reset.pertick",			2000);
 	public static final PluginConfigKey CLAIM_PROTECT_PATHS		= new PluginConfigKey("claim.protect-paths",	false);
 	public static final PluginConfigKey CLAIM_MAX				= new PluginConfigKey("claim.max",				25);
 	public static final PluginConfigKey CLAIM_MAX_UNUSED		= new PluginConfigKey("claim.max-unused",		4);
 	public static final PluginConfigKey CLAIM_COST				= new PluginConfigKey("claim.cost",				0.0d);
-	public static final PluginConfigKey PURGE_SAFE_LIST			= new PluginConfigKey("purge-safe-list",		new String[0]);
 	
 	public static final PluginConfigKey BLOCKS_PATH				= new PluginConfigKey("blocks.path",				Material.DOUBLE_STEP.getId());
 	public static final PluginConfigKey BLOCKS_PATH_DATA		= new PluginConfigKey("blocks.path-data", 			0);
