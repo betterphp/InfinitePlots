89ef0e884ea65c0882ac825f3368b5310394f001
Philippe Plantier <philippe.plantier@gmail.com>
2014-04-27 16:15:40 +0200

    Fixed builder list issues with the new UUID system
    * The builder list was not imported correctly, and broke the plot file
    * The builder list was not serialized properly on file
    * /iplot info was displaying technical information (class name and UUID) instead of player names

--- src/main/java/uk/co/jacekk/bukkit/infiniteplots/plot/PlotManager.java
+++ src/main/java/uk/co/jacekk/bukkit/infiniteplots/plot/PlotManager.java
@@ -65,7 +65,12 @@ public class PlotManager extends BaseObject<InfinitePlots> {
 						
 						try{
 							Map<String, UUID> map = (new UUIDFetcher(builderNames)).call();
-							plotConfig.set(PlotConfig.AUTH_BUILDER_UUIDS, map.values());
+							List<String> builderUuids = new ArrayList<String>();
+							for (UUID uuid: map.values()) {
+								builderUuids.add(uuid.toString());
+							}
+							
+							plotConfig.set(PlotConfig.AUTH_BUILDER_UUIDS, builderUuids);
 							plotConfig.set(PlotConfig.AUTH_BUILDER_NAMES, Arrays.asList());
 						}catch (Exception e){
 							plugin.log.warn("Failed to fetch UUID for plot builders: " + e.getMessage());
