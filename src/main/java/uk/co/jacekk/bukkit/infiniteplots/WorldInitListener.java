1a1c0d8d96e8bbdd6aebdd99078a1e93e8cc9749
Jacek Kuzemczak <jacek@kuzemczak.co.uk>
2016-03-18 19:56:05 +0000

    Use public method for setting the world type instead of reflection

--- src/main/java/uk/co/jacekk/bukkit/infiniteplots/WorldInitListener.java
+++ src/main/java/uk/co/jacekk/bukkit/infiniteplots/WorldInitListener.java
@@ -1,17 +1,14 @@
 package uk.co.jacekk.bukkit.infiniteplots;
 
-import net.minecraft.server.v1_9_R1.WorldData;
-import net.minecraft.server.v1_9_R1.WorldServer;
-import net.minecraft.server.v1_9_R1.WorldType;
-
 import org.bukkit.World;
 import org.bukkit.craftbukkit.v1_9_R1.CraftWorld;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.world.WorldInitEvent;
 
+import net.minecraft.server.v1_9_R1.WorldServer;
+import net.minecraft.server.v1_9_R1.WorldType;
 import uk.co.jacekk.bukkit.baseplugin.event.BaseListener;
-import uk.co.jacekk.bukkit.baseplugin.util.ReflectionUtils;
 import uk.co.jacekk.bukkit.infiniteplots.generation.PlotsGenerator;
 
 public class WorldInitListener extends BaseListener<InfinitePlots> {
@@ -28,15 +25,9 @@ public class WorldInitListener extends BaseListener<InfinitePlots> {
 			this.plugin.getPlotManager().loadPlotsFor(world);
 			
 			WorldServer worldServer = ((CraftWorld) world).getHandle();
+			worldServer.worldData.a(WorldType.FLAT);
 			
-			try{
-				ReflectionUtils.setFieldValue(WorldData.class, "type", worldServer.worldData, WorldType.FLAT);
-				
-				plugin.log.info("Changed the world type of '" + world.getName() + "' to flat (this makes the void blue down to y = 0).");
-			}catch (Exception e){
-				plugin.log.info("Could not change the world type of '" + world.getName() + "'.");
-				e.printStackTrace();
-			}
+			plugin.log.info("Changed the world type of '" + world.getName() + "' to flat (this makes the void blue down to y = 0).");
 		}
 	}
 	
